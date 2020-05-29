1391ac4c9bd542619569bb3065c87d818957884a
Dan Harrin <git@DanHarrin.com>
2020-05-29 07:49:16 +0100

    wip

--- themes/bristol-dwp/resources/tailwind.config.js
+++ themes/bristol-dwp/resources/tailwind.config.js
@@ -1,28 +1,34 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     theme: {
+        extend: {
+            inset: {
+                '20': '5rem',
+                '-36': '-9rem',
+            },
+        },
         fontFamily: {
-            sans: ["Inter", ...defaultTheme.fontFamily.sans]
-        }
+            sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        },
     },
     purge: {
         content: [
-            "./themes/bristol-dwp/**/*.html",
-            "./themes/bristol-dwp/**/*.htm",
-            "./themes/bristol-dwp/**/*.js",
-            "./themes/bristol-dwp/**/*.jsx",
-            "./themes/bristol-dwp/**/*.ts",
-            "./themes/bristol-dwp/**/*.tsx",
-            "./themes/bristol-dwp/**/*.php",
-            "./themes/bristol-dwp/**/*.vue",
-            "./themes/bristol-dwp/**/*.twig"
+            './themes/bristol-dwp/**/*.html',
+            './themes/bristol-dwp/**/*.htm',
+            './themes/bristol-dwp/**/*.js',
+            './themes/bristol-dwp/**/*.jsx',
+            './themes/bristol-dwp/**/*.ts',
+            './themes/bristol-dwp/**/*.tsx',
+            './themes/bristol-dwp/**/*.php',
+            './themes/bristol-dwp/**/*.vue',
+            './themes/bristol-dwp/**/*.twig',
         ],
         options: {
             defaultExtractor: content =>
                 content.match(/[\w-/.:]+(?<!:)/g) || [],
-            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/]
-        }
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+        },
     },
-    plugins: [require("@tailwindcss/ui")]
+    plugins: [require('@tailwindcss/ui')],
 };
